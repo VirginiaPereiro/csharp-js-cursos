fabeb0c76fe57fa26fd4b7e383a9494373415ed3
luisgiltajamar <luis.gil@tajamar365.com>
2014-11-13 12:58:35 +0100

    c

--- ClienteEmpleados/Scripts/proyecto/datos.js
+++ ClienteEmpleados/Scripts/proyecto/datos.js
@@ -4,6 +4,24 @@ function borrarTabla() {
 
 }
 
+function detalleProyecto(id) {
+    var url = url_base + "proyecto/get/" + id;
+
+    $.get(url, function(res) {
+
+        if (res.nombre) {
+            var texto = res.nombre + " del cliente " + res.cliente + "<br/>" + res.descripcion;
+            $("#detalle").html(texto);
+            $("#dlgProyecto").modal();
+
+
+        }
+
+    });
+
+
+}
+
 function escribirFila(dato) {
 
     var fila = "<tr><td>" + dato.nombre + "</td>";
@@ -53,8 +71,83 @@ function buscar() {
     pedirDatosEmpleadosAjax(uri);
 }
 
+function inicializar() {
+
+    var url1 = url_base + "Proyecto/get";
+    $.getJSON(url1, function(res) {
+
+        for (var i = 0; i < res.length; i++) {
+            var txt = "<option value='" + res[i].idProyecto + "'>" +
+                res[i].nombre + "</option>";
+
+            $("#lstProyectos").append(txt);
+
+
+        }
+
+    });
+
+
+    var url2 = url_base + "Cargo/get";
+    $.getJSON(url2, function(res) {
+        
+        for (var i = 0; i < res.length; i++) {
+            var txt = "<option value='" + res[i].idCargo + "'>" +
+                res[i].nombre + "</option>";
+
+            $("#lstCargo").append(txt);
+
+
+        }
+
+
+    });
+
+}
+
+function guardar() {
+    var url = url_base + "empleados";
+    var n = $("#txtNombre").val();
+    var s = $("#txtSalario").val();
+    var c = $("#lstCargo").val();
+    var pd = $("#lstProyectos").find(":selected");
+    var p = [];
+    for (var i = 0; i < pd.length; i++) {
+
+        var d = pd[i];
+        p.push({ idProyecto: parseInt(d.value) });
+
+    }
+
+    var obj = {
+        dni: "12345678", nombre: n, salario: parseFloat(s),
+        idCargo: parseInt(c), proyectos: p
+    };
+    var objtxt = JSON.stringify(obj);
+    $.ajax(url, {
+        method: "POST",
+        data: objtxt,
+        contentType: "application/json",
+        dataType: "json",
+        success: function(res) {
+            pedirDatosEmpleadosAjax("get");
+            $("#dlgAlta").modal('hide');
+
+        }
+    });
+
+
+  
+
+}
+
 (function() {
     pedirDatosEmpleadosAjax("get");
     $("#btnBuscar").bind("click", buscar);
+    $("#btnAdd").click(function() {
+        $("#dlgAlta").modal();
 
+    });
+    $("#btnGuardar").click(guardar);
+    inicializar();
 })();
